aa91879e375d536ce4d134066549e3069d1027f0
bhaskarn.vtalent <bhaskarn.vtalent@gmail.com>
2019-08-27 22:19:26 +0530

    new class added

---
+++ SaiProgram/SaiProgram/Saiprogram.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaiProgram
+{
+   public class Saiprogram
+    {
+        public int mul(int x,int y)
+        {
+            int c = x * y;
+            return c;
+        }
+
+    }
+}
