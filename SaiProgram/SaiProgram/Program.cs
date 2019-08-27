e6af115f26a3a09ffe8d142e7126e34dc8f31f4a
bhaskarn.vtalent <bhaskarn.vtalent@gmail.com>
2019-08-27 22:13:22 +0530

    programs created

--- SaiProgram/SaiProgram/Program.cs
+++ SaiProgram/SaiProgram/Program.cs
@@ -6,10 +6,23 @@ using System.Threading.Tasks;
 
 namespace SaiProgram
 {
-    class Program
+   public class Program
     {
+        public int add(int x,int y)
+        {
+            int c = x + y;
+            return c;
+        }
+
+        public int sub(int x,int y)
+        {
+            int c = x - y;
+            return c;
+        }
         static void Main(string[] args)
         {
+
+
         }
     }
 }
